08c1c176abdf3c4b507efbe523ce8da8863cd5e7
Arimario Jesus <arimario.jesus@hotmail.com>
2021-12-15 20:15:07 -0300

    feat: add Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import { Box, Flex, Icon, Link as ChakraLink } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import Back from '../../assets/icons/back';
+import Logo from '../../assets/images/logo';
+
+export const Header = () => {
+  const { asPath } = useRouter();
+  const outHome = asPath !== '/';
+
+  return (
+    <Flex
+      as="header"
+      align="center"
+      justify="center"
+      w="100%"
+      px="6"
+      maxW={1480}
+      h="90"
+      mx="auto"
+    >
+      {outHome && (
+        <Link href="/" passHref>
+          <ChakraLink justifySelf="left">
+            <Icon as={Back} />
+          </ChakraLink>
+        </Link>
+      )}
+      <Box mx="auto">
+        <Icon as={Logo} />
+      </Box>
+    </Flex>
+  );
+};
